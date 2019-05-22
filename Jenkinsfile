467a100f1bd380267f28d524ba618a5a03241e67
Shone <shonekey@163.com>
2019-05-22 16:52:00 +0800

    update jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,16 +1,3 @@
-import org.foo.groovy.pipeline.utils.*
-
-node{
-    stage('test1'){
-        log.info 'Starting'
-        log.warning 'Nothing to do!'
-    }
-    stage('test2'){
-        log.info 'Starting'
-        log.warning 'Nothing to do!'
-    }
-    stage('test3'){
-        log.info 'Starting'
-        log.warning 'Nothing to do!'
-    }
+buildPlugin{
+    name='stage'
 }
--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -1,3 +1,5 @@
+import org.foo.groovy.pipeline.utils.*
+
 def call(body) {
     // evaluate the body block, and collect configuration into the object
     def config = [:]
@@ -7,8 +9,21 @@ def call(body) {
 
     // now build, based on the configuration provided
     node {
-        git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
-        sh "mvn install"
-        mail to: "...", subject: "${config.name} plugin build", body: "..."
+//        git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
+//        sh "mvn install"
+//        mail to: "...", subject: "${config.name} plugin build", body: "..."
+
+        stage('test1'){
+            log.info 'Starting'
+            log.warning 'Nothing to do!'
+        }
+        stage('test2'){
+            log.info 'Starting'
+            log.warning 'Nothing to do!'
+        }
+        stage('test3'){
+            log.info 'Starting'
+            log.warning 'Nothing to do!'
+        }
     }
 }

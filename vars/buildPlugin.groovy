6f8b93c45bbe16c22d4804e56a6326f081449ada
Shone <shonekey@163.com>
2019-05-24 10:22:32 +0800

    update jenkinsfile

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -1,4 +1,4 @@
-import org.foo.groovy.pipeline.utils.*
+import org.foo.groovy.pipeline.utils
 
 def call(body) {
     //evaluate the body block, and collect configuration into the object
@@ -11,16 +11,16 @@ def call(body) {
     // now build, based on the configuration provided
     node{
         stage('test1'){
-            info 'Starting'
-            warning 'Nothing to do!'
+            utils.info 'Starting'
+            utils.warning 'Nothing to do!'
         }
         stage('test2'){
-            info 'Starting'
-            warning 'Nothing to do!'
+            utils.info 'Starting'
+            utils.warning 'Nothing to do!'
         }
         stage('test3'){
-            info 'Starting'
-            warning 'Nothing to do!'
+            utils.info 'Starting'
+            utils.warning 'Nothing to do!'
         }
     }
 
